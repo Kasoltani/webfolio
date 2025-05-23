a98cf48c0b0e8ce7e069e36b6a44dcdf6925d5da
kasoltani <kassoltani@gmail.com>
2025-05-23 01:13:31 -0700

    saving work

--- appfolio/src/App.jsx
+++ appfolio/src/App.jsx
@@ -1,17 +1,21 @@
 import { useState } from 'react'
 import './App.css'
 
+
 function App() {
   return (
     <>
       <button type="button" onClick={incrButton} className="bg-sky-500 hover:bg-sky-700 rounded h-auto w-auto p-2"> Test </button>
+      <h1>test</h1>
     </>
   );
 }
 
-export default App
-
-
 function incrButton(){
-  
+  fetch('http://127.0.0.1:5000/addNum/')
+    .then(response => console.log(response.json()))
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error))
 }
+
+export default App
--- backend/routes.py
+++ backend/routes.py
@@ -1,14 +1,15 @@
 from flask import Flask, request, jsonify
-
-button_count = 0
+import json
 
 def register_routes(app):
+
     @app.route("/")
     def hello_world():
         return "<h1>Hello, World!</h1>"
     
-    @app.route("/addNum/", methods=['POST'])
+    @app.route("/addNum/", methods=['POST', 'GET'])
     def add_Proj():
         button_count += 1
+        print(button_count)
         return button_count
         
